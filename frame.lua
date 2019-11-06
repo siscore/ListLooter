7b1947baa8f6fa9bfb8df9a42872dcc0427a7f6d
TerekhovAA <terekhovaa@quarta.su>
2019-11-06 13:32:25 +0300

    2.1 Release

--- frame.lua
+++ frame.lua
@@ -4,188 +4,434 @@
 local _, core = ...;
 core.Frame = {}; -- adds Frame table to addon namespace
 
+--------------------------------------
+-- Defaults (usually a database!)
+--------------------------------------
+local defaults = {
+	frame = {
+		iconSize = 22,
+		fontItem = GameFontWhite:GetFont(),
+        fontSizeItem = 12,
+        fontCount = NumberFontNormalSmall:GetFont(),
+        fontSizeCount = 12
+	},
+	framePosition = "",
+	item = {
+		index = 0,
+		lootIcon = 0, 
+		lootName = "", 
+		lootQuantity = 0, 
+		currencyID = nil, 
+		lootQuality = 0, 
+		locked = nil, 
+		isQuestItem = nil, 
+		questID = nil, 
+		isActive = nil
+	},
+	delta = 5
+}
+--------------------------------------
 local Frame = core.Frame;
 local UIFrame;
-local UIDebug;
-local Loot = {};
-local delta = 5;
-local frameH = 40;
-local frameW = 40;
-local iconSize = frameW-1.5*delta;
-local fontItem = GameFontWhite:GetFont()
-local fontSizeItem = 12;
-local fontCount = NumberFontNormalSmall:GetFont()
-local fontSizeCount = 12;
-local frameTop = -1 * delta;
+local m = 0;
 
-function Frame:DebugFrame()
-	local frame = UIDebug or Frame:CreateDebugFrame();
-	frame:SetShown(not frame:IsShown());
+local round = function(n)
+	return math.floor(n * 1e5 + .5) / 1e5;
 end
 
-function Frame:CreateDebugFrame()
-	UIDebug = CreateFrame("Frame","ListLotterDebugFrame",UIParent,"BasicFrameTemplate");
-	UIDebug:SetSize(400,100);
-	UIDebug:SetPoint("CENTER");
-	UIDebug:SetShown(false);
-	
-	UIDebug.button1 = CreateFrame("Button","TestButton1",UIDebug,"UIPanelButtonTemplate");
-	UIDebug.button1:SetSize(300,20);
-	UIDebug.button1:SetPoint("TOP",0,-40);
-	UIDebug.button1:SetText("Emulate Open Loot Event");
-	UIDebug.button1:SetScript("OnClick", function(self, button, down)
-											Frame:AddItemToLoot(-1);
-											Frame:AddItemToLoot(-2);
-											Frame:AddItemToLoot(-3);
-											Frame:AddItemToLoot(-4);
-											Frame:ShowLootFrame();
-										 end);
-	
-	UIDebug.button2 = CreateFrame("Button","TestButton1",UIDebug,"UIPanelButtonTemplate");
-	UIDebug.button2:SetSize(300,20);
-	UIDebug.button2:SetPoint("TOP",0,-65);
-	UIDebug.button2:SetText("Emulate Close Loot Event");
-	UIDebug.button2:SetScript("OnClick", function(self, button, down)
-												Frame:LootClosed();
-										 end);
-	
-	return UIDebug;
+function Frame:SavePosition()
+	local point, parent, _, x, y = UIFrame:GetPoint();
+
+	defaults.framePosition = string.format(
+		'%s\031%s\031%d\031%d',
+		point, 'UIParent', round(x), round(y)
+	);
 end
 
-function Frame:CreateLootFrame()
-	local lootCount = table.getn(Loot);
-	UIFrame = CreateFrame("Frame","ListLotterLootFrame",UIParent)
-	UIFrame:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 100, -100);
-	UIFrame:SetSize(frameH + 2*delta, lootCount*frameW-((lootCount-3)*delta));
+function Frame:LoadPosition()
+	local scale = UIFrame:GetScale();
+	local point, parentName, x, y = string.split('\031', defaults.framePosition);
+
+	UIFrame:ClearAllPoints();
+	UIFrame:SetPoint(point, parentName, point, x / scale, y / scale);
+end
+	
+function Frame:Init()
+	UIFrame = CreateFrame("Button", "ListLooterLootFrame", UIParent)
+	UIFrame:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 2*defaults.delta, -2*defaults.delta);
+	UIFrame:SetSize(1,1);
 	UIFrame:SetShown(false);
-	UIFrame:SetBackdrop({bgFile = "Interface/Tooltips/UI-Tooltip-Background", 
-                                            edgeFile = "Interface/Tooltips/UI-Tooltip-Border", 
-                                            tile = true, tileSize = 16, edgeSize = 16, 
-                                            insets = { left = 4, right = 4, top = 4, bottom = 4 }});
-	UIFrame:SetBackdropColor(0,0,0,0.8);
-	UIFrame.Items = {};
 	
-	frameTop = -1 * delta;
+	UIFrame:SetMovable(true)
+	UIFrame:RegisterForClicks"anyup"
+
+	UIFrame:SetBackdrop{
+		bgFile = "Interface\\Tooltips\\UI-Tooltip-Background", tile = true, tileSize = 16,
+		edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border", edgeSize = 16,
+		insets = {left = 4, right = 4, top = 4, bottom = 4},
+	}
+	UIFrame:SetBackdropColor(0, 0, 0, 0.8)
+
+	UIFrame:SetClampedToScreen(true)
+	UIFrame:SetClampRectInsets(0, 0, 14, 0)
+	UIFrame:SetHitRectInsets(0, 0, -14, 0)
+	UIFrame:SetFrameStrata"HIGH"
+	UIFrame:SetToplevel(true)
 	
-	for c=1, lootCount, 1 do
-		local lootIcon, lootName, lootQuantity, currencyID, lootQuality, locked, isQuestItem, questID, isActive = GetLootSlotInfo(Loot[c]);
-		------------------------
-		if Loot[c]<0 then 
-			lootIcon, lootName, lootQuantity, currencyID, lootQuality, locked, isQuestItem, questID, isActive = 134371,"Испорченный мех",c,nil,0,nil,nil,nil,nil
+	UIFrame:SetScript("OnMouseDown", function(self)
+		if(IsShiftKeyDown()) then
+			self:StartMoving();
 		end
-		------------------------
-		
-		
-		local itemFrame = UIFrame.Items[c] or Frame:CreateItem();
-		
-		local color = ITEM_QUALITY_COLORS[lootQuality]
+	end)
+
+	UIFrame:SetScript("OnMouseUp", function(self)
+		self:StopMovingOrSizing();
+		Frame:SavePosition();
+	end)
+
+	UIFrame:SetScript("OnHide", function(self)
+		StaticPopup_Hide"CONFIRM_LOOT_DISTRIBUTION"
+		CloseLoot();
+	end)
+	
+	UIFrame.Items = {};
+end
+
+function Frame:GetEmptyItem()
+	return defaults.item;
+end
+
+function Frame:AddItem(...)
+	local lootInfo = {...};
+	local item = Frame:GetEmptyItem();
+	
+	item.index = lootInfo[1];
+	item.lootIcon = lootInfo[2];
+	item.lootName = lootInfo[3];
+	item.lootQuantity = lootInfo[4];
+	item.currencyID = lootInfo[5];
+	item.lootQuality = lootInfo[6]; 
+	item.locked = lootInfo[7];
+	item.isQuestItem = lootInfo[8];
+	item.questID = lootInfo[9];
+	item.isActive = lootInfo[10];
+	
+	local itemFrame = UIFrame.Items[item.index];
+	
+	if (not itemFrame) then
+		itemFrame = Frame:CreateItemFrame();
+		table.insert(UIFrame.Items, itemFrame);
+	end 
+	
+	itemFrame.index = item.index;
+	
+	if (item.currencyID) then
+		item.lootName, item.lootIcon, item.lootQuantity, item.lootQuality = CurrencyContainerUtil.GetCurrencyContainerInfo(item.currencyID, item.lootQuantity, item.lootName, item.lootIcon, item.lootQuality);
+	end
+	
+	if(item.lootIcon) then 
+		local color = ITEM_QUALITY_COLORS[item.lootQuality]
 		local r, g, b = color.r, color.g, color.b
-				
-		itemFrame.index = c;
-		itemFrame.icon:SetTexture(lootIcon);
-		itemFrame.name:SetText(lootName)
-		itemFrame.name:SetTextColor(r, g, b)
-		if(lootQuantity > 1) then
-			itemFrame.count:SetText(lootQuantity)
+
+		local slotType = GetLootSlotType(item.index)
+		if(slotType == LOOT_SLOT_MONEY) then
+			item = item:gsub("\n", ", ")
+		end
+
+		if(item.lootQuantity > 1) then
+			itemFrame.count:SetText(item.lootQuantity)
 			itemFrame.count:Show()
 		else
 			itemFrame.count:Hide()
 		end
+
+		if(item.questID and not item.isActive) then
+			itemFrame.quest:Show()
+		else
+			itemFrame.quest:Hide()
+		end
+
+		if(item.lootQuality > 1 or item.questID or item.isQuestItem) then
+			if(item.questID or item.isQuestItem) then
+				r, g, b = 1, 1, .2
+			end
+
+			itemFrame.drop:SetVertexColor(r, g, b)
+			itemFrame.drop:Show()
+		else
+			itemFrame.drop:Hide()
+		end
+
+		itemFrame.isQuestItem = item.isQuestItem
+		itemFrame.quality = item.lootQuality
+
+		itemFrame.name:SetText(item.lootName)
+		itemFrame.name:SetTextColor(r, g, b)
+		itemFrame.icon:SetTexture(item.lootIcon)
+
+		m = math.max(m, item.lootQuality)
+
+		itemFrame:Enable()
+		itemFrame:Show()
+	end 
+end
+
+function Frame:CreateItemFrame(id)
+	local iconSize = ListLooterDB.frame.iconSize;
+	local fontSizeItem = ListLooterDB.frame.fontSizeItem;
+	local fontSizeCount = ListLooterDB.frame.fontSizeCount;
+	local fontItem = defaults.frame.fontItem;
+	local fontCount = defaults.frame.fontCount;
+	local posId = table.getn(UIFrame.Items)+1;
+	
+	local frame = CreateFrame("Button", "ListLooterLootFrameItem"..posId, UIFrame);
+	frame:SetHeight(math.max(fontSizeItem, iconSize));
+	frame:SetID(posId);
+
+	frame:RegisterForClicks("LeftButtonUp", "RightButtonUp");
+
+	frame:SetScript("OnEnter", function(self)
+		local slot = self.index;
+		if(GetLootSlotType(slot) == LOOT_SLOT_ITEM) then
+			GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+			GameTooltip:SetLootItem(slot)
+			CursorUpdate(self)
+		end
+		if(self.drop:IsShown()) then
+			local r, g, b = self.drop:GetVertexColor()
+			self.drop:SetVertexColor(r * .6, g * .6, b * .6)
+		else
+			self.drop:SetVertexColor(1, 1, 0)
+		end
+
+		self.drop:Show()
+	end);
+	
+	frame:SetScript("OnLeave", function(self)
+		if(self.quality > 1) then
+			local color = ITEM_QUALITY_COLORS[self.quality]
+			self.drop:SetVertexColor(color.r, color.g, color.b)
+		elseif(self.isQuestItem) then
+			self.drop:SetVertexColor(1, 1, .2)
+		else
+			self.drop:Hide()
+		end
 		
-		itemFrame:SetShown(true);
+		GameTooltip:Hide()
+		ResetCursor()
+	end);
+	
+	frame:SetScript("OnClick", function(self)
+		if(IsModifiedClick()) then
+			HandleModifiedItemClick(GetLootSlotLink(self.index))
+			if (IsAltKeyDown()) then
+				core.Config.AddItemByLink("link",GetLootSlotLink(self.index));
+			end 
+		else
+			StaticPopup_Hide"CONFIRM_LOOT_DISTRIBUTION";
+
+			LootFrame.selectedLootButton = self;
+			LootFrame.selectedSlot = self.index;
+			LootFrame.selectedQuality = self.quality;
+			LootFrame.selectedItemName = self.name:GetText();
+
+			LootSlot(self.index); 
+		end
+	end);
+	
+	frame:SetScript("OnUpdate", function(self)
+		if(GameTooltip:IsOwned(self)) then
+			GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+			GameTooltip:SetLootItem(self.index)
+			CursorOnUpdate(self)
+		end
+	end);
+
+	local iconFrame = CreateFrame("Frame", "ListLooterLootFrameItemIcon", frame);
+	iconFrame:SetSize(iconSize, iconSize);
+	iconFrame:SetPoint("RIGHT", frame);
+	frame.iconFrame = iconFrame;
+
+	local icon = iconFrame:CreateTexture(nil, "ARTWORK")
+	icon:SetAlpha(.8)
+	icon:SetTexCoord(.07, .93, .07, .93)
+	icon:SetAllPoints(iconFrame)
+	frame.icon = icon
+
+	local quest = iconFrame:CreateTexture(nil, 'OVERLAY')
+	quest:SetTexture([[Interface\Minimap\ObjectIcons]])
+	quest:SetTexCoord(1/8, 2/8, 1/8, 2/8)
+	quest:SetSize(iconSize * .8, iconSize * .8)
+	quest:SetPoint('BOTTOMLEFT', -iconSize * .15, 0)
+	frame.quest = quest
+
+	local count = iconFrame:CreateFontString(nil, "OVERLAY")
+	count:SetJustifyH"RIGHT"
+	count:SetPoint("BOTTOMRIGHT", iconFrame, 2, 2)
+	count:SetFont(fontCount, fontSizeCount, 'OUTLINE')
+	count:SetShadowOffset(.8, -.8)
+	count:SetShadowColor(0, 0, 0, 1)
+	count:SetText(1)
+	frame.count = count
+
+	local name = frame:CreateFontString(nil, "OVERLAY")
+	name:SetJustifyH"LEFT"
+	name:SetPoint("LEFT", frame)
+	name:SetPoint("RIGHT", iconFrame, "LEFT")
+	name:SetNonSpaceWrap(true)
+	name:SetFont(fontItem, fontSizeItem)
+	name:SetShadowOffset(.8, -.8)
+	name:SetShadowColor(0, 0, 0, 1)
+	frame.name = name
+
+	local drop = frame:CreateTexture(nil, "ARTWORK")
+	drop:SetTexture[[Interface\QuestFrame\UI-QuestLogTitleHighlight]]
+
+	drop:SetPoint("LEFT", icon, "RIGHT")
+	drop:SetPoint("RIGHT", frame)
+	drop:SetAllPoints(frame)
+	drop:SetAlpha(.3)
+	frame.drop = drop
+	
+	return frame
+end
+
+function Frame:ShowLootList()
+	if (Frame:GetVisibleListItems() == 0) then return end;
+	Frame:AnchorItemsFrames();
+	
+	local color = ITEM_QUALITY_COLORS[m];
+	UIFrame:SetBackdropBorderColor(color.r, color.g, color.b, .8);
+	
+	Frame:UpdateWidth();
+	
+	-- Blizzard uses strings here
+	if(GetCVar("lootUnderMouse") == "1") then
+		local x, y = GetCursorPosition();
+		x = x / UIFrame:GetEffectiveScale();
+		y = y / UIFrame:GetEffectiveScale();
+
+		UIFrame:ClearAllPoints();
+		UIFrame:SetPoint("TOPLEFT", nil, "BOTTOMLEFT", x-40, y+20);
+		UIFrame:GetCenter();
+		UIFrame:Raise();
 	end
 	
-	local maxItemWidth = 0
-	for id = 1, table.getn(UIFrame.Items), 1 do
-		local width = UIFrame.Items[id].name:GetWidth() + UIFrame.Items[id].iconFrame:GetWidth() + 4*delta;
-		if (width > maxItemWidth) then 
-			maxItemWidth = width;
+	UIFrame:Show();
+end
+
+function Frame:GetVisibleListItems()
+	local result = 0;
+	for i=1, table.getn(UIFrame.Items) do
+		local frame = UIFrame.Items[i]
+		if(frame:IsShown()) then
+			result = result + 1;
+		end
+	end
+	return result;
+end
+
+function Frame:AnchorItemsFrames()
+	local frameSize = math.max(defaults.frame.iconSize, defaults.frame.fontSizeItem);
+	local iconSize = defaults.frame.iconSize;
+	local shownSlots = 0;
+
+	local prevShown;
+	for i=1, table.getn(UIFrame.Items) do
+		local frame = UIFrame.Items[i]
+		if(frame:IsShown()) then
+			frame:ClearAllPoints()
+			frame:SetPoint("LEFT", 8, 0)
+			frame:SetPoint("RIGHT", -8, 0)
+			if(not prevShown) then
+				frame:SetPoint('TOPLEFT', UIFrame, 8, -8)
+			else
+				frame:SetPoint('TOP', prevShown, 'BOTTOM')
+			end
+
+			frame:SetHeight(frameSize)
+			shownSlots = shownSlots + 1
+			prevShown = frame
+		end
+	end
+
+	local offset = UIFrame:GetTop() or 0
+	UIFrame:SetHeight(math.max((shownSlots * frameSize + 16), 20))
+
+	-- Reposition the frame so it doesn't move.
+	local point, parent, relPoint, x, y = UIFrame:GetPoint()
+	offset = offset - (UIFrame:GetTop() or 0)
+	UIFrame:SetPoint(point, parent, relPoint, x, y + offset)
+end
+
+function Frame:UpdateWidth()
+	local maxWidth = 0;
+	for i=1, table.getn(UIFrame.Items) do
+		local frame = UIFrame.Items[i];
+		if(frame:IsShown()) then
+			local width = frame.name:GetStringWidth()
+			if(width > maxWidth) then
+				maxWidth = width;
+			end
+		end
+	end
+
+	UIFrame:SetWidth(maxWidth + 30 + defaults.frame.iconSize);
+end
+
+function Frame:CloseLootFrame()
+	StaticPopup_Hide"LOOT_BIND";
+	UIFrame:Hide();
+
+	for i=1, table.getn(UIFrame.Items) do
+		UIFrame.Items[i]:Hide();
+	end
+	
+	m = 0;
+end
+
+function Frame:LootFrameItemCleared(index)
+	if(not UIFrame:IsShown()) then return end
+	for i=1, table.getn(UIFrame.Items) do
+		local frame = UIFrame.Items[i];
+		if (frame.index == index) then 
+			frame:Hide();
+			Frame:AnchorItemsFrames();
+			return;
 		end 
-	end 
-	for id = 1, table.getn(UIFrame.Items), 1 do
-		UIFrame.Items[id]:SetWidth(maxItemWidth);
-	end 
-	
-	UIFrame:SetWidth(maxItemWidth + 2*delta);
-	
-	return UIFrame;
-end 
-
-function Frame:CreateItem()
-	local posId = table.getn(UIFrame.Items)+1;
-		
-	if (posId > 1) then 
-		frameTop = frameTop - frameW + delta;
-	end	
-	
-	local itemFrame = CreateFrame("Button","ListLotterLootFrameItem"..posId,UIFrame);
-	itemFrame:SetShown(false);
-	itemFrame.index = 0;
-	itemFrame:SetPoint("TOP", UIFrame, "TOP", 0, frameTop);
-	itemFrame:SetSize(frameH, frameW);
-	itemFrame:SetScript("OnClick", function(self, button, down)
-									Frame:LootItem(self);
-									self:SetShown(false);
-								end);
-	
-	local iconFrame = CreateFrame("Frame","ListLotterLootFrameItemIco",itemFrame);
-	iconFrame:SetSize(iconSize, iconSize);
-	iconFrame:SetPoint("TOPLEFT", itemFrame, delta, -delta);
-	itemFrame.iconFrame = iconFrame;
-	
-	local icon = itemFrame:CreateTexture(nil,"ARTWORK");
-	icon:SetAlpha(.8);
-	icon:SetTexCoord(.07, .93, .07, .93);
-	icon:SetAllPoints(iconFrame);
-	icon:SetTexture(0);
-	itemFrame.icon = icon;
-	
-	local count = iconFrame:CreateFontString(nil, "OVERLAY");
-	count:SetJustifyH"RIGHT";
-	count:SetPoint("BOTTOMRIGHT", iconFrame, 2, 2);
-	count:SetFont(fontCount, fontSizeCount, 'OUTLINE');
-	count:SetShadowOffset(.8, -.8);
-	count:SetShadowColor(0, 0, 0, 1);
-	count:SetText("");
-	itemFrame.count = count;
-	
-	local name = itemFrame:CreateFontString(nil, "OVERLAY");
-	name:SetJustifyH"LEFT";
-	name:SetPoint("LEFT", itemFrame, "LEFT", iconSize + 2*delta, 0);
-	name:SetNonSpaceWrap(true);
-	name:SetFont(fontItem, fontSizeItem);
-	name:SetShadowOffset(.8, -.8);
-	name:SetShadowColor(0, 0, 0, 1);
-	name:SetText("");
-	itemFrame.name = name;
-	itemFrame:SetWidth(name:GetWidth() + iconSize + 4*delta);
-	table.insert(UIFrame.Items, itemFrame);
-	
-	return itemFrame;
-end 
-
-function Frame:ShowLootFrame()
-	local lootExist = (table.getn(Loot) > 0);
-	if (lootExist) then 
-		local frame = UIFrame or Frame:CreateLootFrame();
-		frame:SetShown(not frame:IsShown());
-	end 	
-end 
-
-function Frame:AddItemToLoot(...)
-	local lootInfo = {...};
-	table.insert(Loot, lootInfo[1]);
-end 
-
-function Frame:LootItem(self, button)
-	LootSlot(self.index);
-end 
-
-function Frame:LootClosed()
-	local lootExist = (table.getn(Loot) > 0);
-	if (lootExist) then 
-		Loot = {};
-		UIFrame:SetShown(false);
-		UIFrame = nil;
 	end
 end
+
+function Frame:UpdateSettings()
+	for i=1, table.getn(UIFrame.Items) do
+		local frame = UIFrame.Items[i];
+		
+		local fontName, sizeName, outlineName = frame.name:GetFont();
+		frame.name:SetFont(fontName, ListLooterDB.frame.fontSizeItem, outlineName);
+		
+		local fontCount, sizeCount, outlineCount = frame.count:GetFont();
+		frame.count:SetFont(fontCount, ListLooterDB.frame.fontSizeCount, outlineCount);
+		
+		frame:SetHeight(ListLooterDB.frame.iconSize);
+		frame.iconFrame:SetSize(ListLooterDB.frame.iconSize, ListLooterDB.frame.iconSize);
+
+		frame.quest:SetSize(ListLooterDB.frame.iconSize * .8, ListLooterDB.frame.iconSize * .8);
+		frame.quest:ClearAllPoints();
+		frame.quest:SetPoint('BOTTOMLEFT', -ListLooterDB.frame.iconSize * .15, 0);
+		
+		Frame:UpdateWidth();
+		Frame:AnchorItemsFrames();
+	end
+end
+
+function Frame:HideBlizzardLootFrame(isHide)
+	if (isHide) then 
+		LootFrame:UnregisterEvent("LOOT_OPENED");
+		LootFrame:UnregisterEvent("LOOT_CLOSED");
+		LootFrame:UnregisterEvent("LOOT_SLOT_CLEARED");
+	else
+		LootFrame:RegisterEvent("LOOT_OPENED");
+		LootFrame:RegisterEvent("LOOT_CLOSED");
+		LootFrame:RegisterEvent("LOOT_SLOT_CLEARED");
+	end 
+end 
