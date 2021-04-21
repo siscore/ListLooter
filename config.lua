d211d1581b7899d27ca2cdadd06c79be6bc4e05c
Терехов Андрей <terehovaa@quarta.su>
2021-04-21 10:59:38 +0300

    improve list of items and LSM detection

--- config.lua
+++ config.lua
@@ -11,7 +11,7 @@ local UIConfig;
 --------------------------------------
 local defaults = {
 	settings = {
-		isLootEnable = true, 
+		isLootEnable = true,
 		isCurrency = false,
 		isQuestItem = false,
 		isAfterClose = false,
@@ -21,7 +21,7 @@ local defaults = {
 		customFontName = "Default",
 	},
 	theme = {
-		r = 0, 
+		r = 0,
 		g = 0.8, -- 204/255
 		b = 1,
 		hex = "00ccff"
@@ -41,7 +41,7 @@ local actions = {
 
 local itemsDB = {};
 --itemsDB = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50};
-
+local previouslyItemListRow = nil;
 --------------------------------------
 -- Config functions
 --------------------------------------
@@ -50,7 +50,7 @@ function Config:OnEvent(event, name)
 		--print("Call: "..name);
 		Config:ItemInfoReceived(name);
 	end
-end 
+end
 
 function Config:Toggle()
 	local menu = UIConfig or Config:CreateMenu();
@@ -58,11 +58,12 @@ function Config:Toggle()
 end
 
 function Config:LSMDetected()
-	UIConfig.ddCustomFont:Hide();
+	local menu = UIConfig or Config:CreateMenu();
+	menu.ddCustomFont:Hide();
 	local fontsList = core.FontProvider:GetFontsName();
 	local font_opts = {
 		["name"]  ="custom_font_name",
-		["parent"] = UIConfig,
+		["parent"] = menu,
 		["title"] = L_OPTIONS_CUSTOMFONTNAME,
 		["items"] = fontsList,
 		["defaultVal"] = ListLooterDB.settings.customFontName or "Default",
@@ -71,8 +72,8 @@ function Config:LSMDetected()
 			core.Frame:UpdateSettings();
 		end
 	}
-	UIConfig.ddCustomFont = Config:CreateDropdown(font_opts);
-	UIConfig.ddCustomFont:SetPoint("TOPLEFT", UIConfig.cbAfterClose, "BOTTOMLEFT", -12, -15);
+	menu.ddCustomFont = Config:CreateDropdown(font_opts);
+	menu.ddCustomFont:SetPoint("TOPLEFT", menu.cbAfterClose, "BOTTOMLEFT", -12, -15);
 end
 
 function Config:ToggleConfig()
@@ -94,10 +95,10 @@ function Config:ItemInfoReceived(itemId)
 		Config:GetListFromGlobal();
 		Config:CreateContent(UIConfig.list.listFrame.ScrollFrame.content);
 	end
-	if (actions.add) then 
+	if (actions.add) then
 		actions.add=false;
 		Config:AddItem(itemId);
-	end 
+	end
 end
 
 function Config:GetThemeColor()
@@ -138,26 +139,25 @@ function Config:CreatePointer(relativeFrame, yOffset, text)
     pointer.right:SetTexture("Interface\\Tooltips\\UI-Tooltip-Border");
     pointer.right:SetTexCoord(0.81, 0.94, 0.5, 1);
     pointer.left:SetPoint("RIGHT", pointer.label, "LEFT", -5, 0);
-    
+
 	return pointer;
-end 
+end
 
 function Config:CreateEditBox(relativeFrame, yOffset, focus)
 	local parent = relativeFrame:GetParent();
 	local editBox = core.Override.CreateFrameA(nil, "EditBox", nil, relativeFrame, "InputBoxTemplate");
 	editBox:SetPoint("TOPLEFT", relativeFrame, "TOPLEFT", 40, yOffset);
 	editBox:SetSize(parent:GetWidth() - 90, 18);
-	editBox:SetAutoFocus(focus); 
-	
+	editBox:SetAutoFocus(focus);
+
 	return editBox;
 end
 
 function Config:CreateTableRow(parent, rowHeight, N)
 	--print("CreateTableRow");
-	
+
 	local fontHeight = select(2, GameFontNormalSmall:GetFont());
 	local rowHeight = fontHeight + 6;
-	
 	local row = core.Override.CreateFrameA(nil, "Button", nil, parent);
 	row:SetHighlightTexture("Interface\\QuestFrame\\UI-QuestTitleHighlight", "ADD");
 	row.id = N;
@@ -166,15 +166,18 @@ function Config:CreateTableRow(parent, rowHeight, N)
 	row:SetNormalFontObject("GameFontNormal")
 	row:SetScript("OnEnter", function()
 			-- ** HgD CODE CHANGES START HERE ** --
+			if (previouslyItemListRow and row.id ~= previouslyItemListRow.id) then
+				previouslyItemListRow.delete:Hide();
+			end 
 			row.delete:Show();
 			-- ** HgD CODE CHANGES END HERE ** --
 		end);
 
 	row:SetScript("OnLeave", function()
 			-- ** HgD CODE CHANGES START HERE ** --
-			row.delete:Hide();
+			previouslyItemListRow = row;
 			-- ** HgD CODE CHANGES END HERE ** --
-		end);	
+		end);
 
 	row.font = row:CreateFontString(nil, "BACKGROUND", "GameFontNormal");
 	row.font:SetAllPoints()
@@ -182,8 +185,8 @@ function Config:CreateTableRow(parent, rowHeight, N)
 	row.font:SetJustifyH("LEFT");
 
 	row.delete = core.Override.CreateFrameA(nil, "Button", nil, row, "UIPanelCloseButton");
-	row.delete:SetHeight(rowHeight*2);
-	row.delete:SetWidth(rowHeight*2);
+	row.delete:SetHeight(rowHeight*1.5);
+	row.delete:SetWidth(rowHeight*1.5);
 	row.delete:SetPoint("RIGHT", row, "RIGHT", -10, 0);
 	row.delete:Hide();
 	row.delete:RegisterForClicks("LeftButtonUp");
@@ -195,16 +198,14 @@ function Config:CreateTableRow(parent, rowHeight, N)
 	row.delete:SetScript("OnEnter", function()
 			-- ** HgD CODE CHANGES START HERE ** --
 			row:LockHighlight();
-			row.delete:Show();
 			-- ** HgD CODE CHANGES END HERE ** --
 		end);
 	row.delete:SetScript("OnLeave", function()
 			-- ** HgD CODE CHANGES START HERE ** --
 			row:UnlockHighlight();
-			row.delete:Hide();
 			-- ** HgD CODE CHANGES END HERE ** --
 		end);
-	
+
 	return row
 end
 
@@ -217,12 +218,12 @@ end
 
 function Config:CreateContent(content)
 	--print("CreateContent");
-	
+
 	for i=1,table.getn(content.rows) do
 		local r = content.rows[i];
 		r:Hide();
-	end 	
-	
+	end
+
 	for i=1,table.getn(itemsDB) do
 		if (content.rows[i] == nil) then
 			--Create new
@@ -238,18 +239,18 @@ function Config:CreateContent(content)
 			local r = content.rows[i];
 			r.font:SetText(itemsDB[i].name);
 			r:Show();
-		end 
+		end
 	end
 end
 
 function Config:AddItem(inputItem)
 	--print("Config:AddItem("..inputItem..")")
 	local _, itemLink = GetItemInfo(inputItem)
-		
-	if itemLink ~= nil then 
+
+	if itemLink ~= nil then
 		local _, _, Id = string.find(itemLink, "item:(%d+):")
-			
-		if Id then 
+
+		if Id then
 			table.insert(ListLooterDB.LootDB,Id);
 			self:GetListFromGlobal();
 			self:CreateContent(UIConfig.list.listFrame.ScrollFrame.content);
@@ -261,31 +262,31 @@ end
 
 local function ScrollFrame_OnMouseWheel(self, delta)
 	local newValue = self:GetVerticalScroll() - (delta * 20);
-	
+
 	if (newValue < 0) then
 		newValue = 0;
 	elseif (newValue > self:GetVerticalScrollRange()) then
 		newValue = self:GetVerticalScrollRange();
 	end
-	
+
 	self:SetVerticalScroll(newValue);
 end
 
 function Config:GetListFromGlobal()
 	--print("GetListFromGlobal");
 	itemsDB={};
-	
+
 	for i=1,table.getn(ListLooterDB.LootDB) do
 		local itemName, itemLink = GetItemInfo(ListLooterDB.LootDB[i])
 		local itemIcon = GetItemIcon(ListLooterDB.LootDB[i])
 		local showName = ListLooterDB.LootDB[i];
-		
-		if (itemName ~= nil) then 
+
+		if (itemName ~= nil) then
 			showName = "\124T"..itemIcon..":0\124t".." "..itemName.." ("..ListLooterDB.LootDB[i]..")"
 		else
 			actions.read=true;
 		end
-		
+
 		table.insert(itemsDB, {id=ListLooterDB.LootDB[i], name=showName});
 	end
 end
@@ -295,7 +296,7 @@ function Config:UpdateSettings1()
 		local value1, value2, value3, value4, value5, value6 = ListLooterDB.settings.isEnable, ListLooterDB.settings.isCurrency, ListLooterDB.settings.isQuestItem, ListLooterDB.settings.isAfterClose, ListLooterDB.settings.isMinimap, ListLooterDB.settings.isLootFrame
 
 		ListLooterDB.settings = {
-			isLootEnable = value1, 
+			isLootEnable = value1,
 			isCurrency = value2,
 			isQuestItem = value3,
 			isAfterClose = value4,
@@ -310,13 +311,13 @@ function Config:UpdateSettings2()
 		local value1, value2, value3, value4, value5, value6 = ListLooterDB.settings.isLootEnable, ListLooterDB.settings.isCurrency, ListLooterDB.settings.isQuestItem, ListLooterDB.settings.isAfterClose, ListLooterDB.settings.isMinimap, ListLooterDB.settings.isLootFrame
 
 		ListLooterDB.settings = {
-			isLootEnable = value1, 
+			isLootEnable = value1,
 			isCurrency = value2,
 			isQuestItem = value3,
 			isAfterClose = value4,
 			isMinimap = value5,
 			isLootFrame = value6,
-			isFishingLoot = defaults.settings.isFishingLoot	
+			isFishingLoot = defaults.settings.isFishingLoot
 		};
 	end
 end
@@ -333,13 +334,13 @@ function Config:CreateMenu()
 	if ListLooterDB == nil then
 		ListLooterDB = {};
 		ListLooterDB.settings = {
-			isLootEnable = defaults.settings.isLootEnable, 
+			isLootEnable = defaults.settings.isLootEnable,
 			isCurrency = defaults.settings.isCurrency,
 			isQuestItem = defaults.settings.isQuestItem,
 			isAfterClose = defaults.settings.isAfterClose,
 			isMinimap = defaults.settings.isMinimap,
 			isLootFrame = defaults.settings.isLootFrame,
-			isFishingLoot = defaults.settings.isFishingLoot	
+			isFishingLoot = defaults.settings.isFishingLoot
 		};
 		ListLooterDB.LootDB = {};
 	end
@@ -347,8 +348,8 @@ function Config:CreateMenu()
 	Config:UpdateSettings1();
 	Config:UpdateSettings2();
 	Config:UpdateSettings3();
-	
-	if (ListLooterDB.frame == nil) then 
+
+	if (ListLooterDB.frame == nil) then
 		ListLooterDB.frame = defaults.frame;
 	end
 
@@ -358,14 +359,14 @@ function Config:CreateMenu()
 	----------------------------------
 	UIConfig = core.Override.CreateFrameA(nil, 'Frame', 'ListLooterConfig', InterfaceOptionsFramePanelContainer);
 	UIConfig.name = "|cff00ccffList Looter|r";
-	
+
 	UIConfig.title = UIConfig:CreateFontString(nil, "BACKGROUND", "GameFontNormalLarge");
     UIConfig.title:SetPoint("TOPLEFT", UIConfig, "TOPLEFT", 40, -20);
     UIConfig.title:SetText("|cff00ccffList Looter|r");
-	
+
 	UIConfig.poiner1 = self:CreatePointer(UIConfig, -50, L_OPTIONS_MAINSETTINGS);
 	UIConfig.poiner1:SetWidth(550);
-	
+
 	----------------------------------
 	-- Check Buttons
 	----------------------------------
@@ -374,7 +375,7 @@ function Config:CreateMenu()
 	UIConfig.cbEnable:SetPoint("TOPLEFT", UIConfig, "TOPLEFT", 40, -70);
 	UIConfig.cbEnable.text:SetText(L_OPTIONS_ENABLE);
 	UIConfig.cbEnable:SetChecked(ListLooterDB.settings.isLootEnable);
-	UIConfig.cbEnable:SetScript("OnClick", function(self, button, down) 
+	UIConfig.cbEnable:SetScript("OnClick", function(self, button, down)
 												ListLooterDB.settings.isLootEnable = self:GetChecked() and true or false;
 										   end);
 
@@ -383,25 +384,25 @@ function Config:CreateMenu()
 	UIConfig.cbCurrency:SetPoint("TOPLEFT", UIConfig.cbEnable, "BOTTOMLEFT", 0, -5);
 	UIConfig.cbCurrency.text:SetText(L_OPTIONS_CURRENCY);
 	UIConfig.cbCurrency:SetChecked(ListLooterDB.settings.isCurrency);
-    UIConfig.cbCurrency:SetScript("OnClick", function(self, button, down) 
+    UIConfig.cbCurrency:SetScript("OnClick", function(self, button, down)
 												ListLooterDB.settings.isCurrency = self:GetChecked() and true or false;
 										   end);
-	
+
 	-- Check Button 3:
 	UIConfig.cbQuestItems = core.Override.CreateFrameA(nil, "CheckButton", nil, UIConfig, "UICheckButtonTemplate");
 	UIConfig.cbQuestItems:SetPoint("TOPLEFT", UIConfig.cbCurrency, "BOTTOMLEFT", 0, -5);
 	UIConfig.cbQuestItems.text:SetText(L_OPTIONS_QUESTITEMS);
 	UIConfig.cbQuestItems:SetChecked(ListLooterDB.settings.isQuestItem);
-    UIConfig.cbQuestItems:SetScript("OnClick", function(self, button, down) 
+    UIConfig.cbQuestItems:SetScript("OnClick", function(self, button, down)
 												ListLooterDB.settings.isQuestItem = self:GetChecked() and true or false;
 										   end);
-	
+
 	-- Check Button 5:
 	UIConfig.cbFishingLoot = core.Override.CreateFrameA(nil, "CheckButton", nil, UIConfig, "UICheckButtonTemplate");
 	UIConfig.cbFishingLoot:SetPoint("TOPLEFT", UIConfig.cbQuestItems, "BOTTOMLEFT", 0, -5);
 	UIConfig.cbFishingLoot.text:SetText(L_OPTIONS_FISHLOOT);
 	UIConfig.cbFishingLoot:SetChecked(ListLooterDB.settings.isFishingLoot);
-	UIConfig.cbFishingLoot:SetScript("OnClick", function(self, button, down) 
+	UIConfig.cbFishingLoot:SetScript("OnClick", function(self, button, down)
 												ListLooterDB.settings.isFishingLoot = self:GetChecked() and true or false;
 											end);
 	-- Check Button 4:
@@ -409,18 +410,18 @@ function Config:CreateMenu()
 	UIConfig.cbAfterClose:SetPoint("TOPLEFT", UIConfig.cbFishingLoot, "BOTTOMLEFT", 0, -5);
 	UIConfig.cbAfterClose.text:SetText(L_OPTIONS_AFTERCLOSE);
 	UIConfig.cbAfterClose:SetChecked(ListLooterDB.settings.isAfterClose);
-    UIConfig.cbAfterClose:SetScript("OnClick", function(self, button, down) 
+    UIConfig.cbAfterClose:SetScript("OnClick", function(self, button, down)
 												ListLooterDB.settings.isAfterClose = self:GetChecked() and true or false;
 										   end);
 
-	-- Drop Down Button 1:									   
+	-- Drop Down Button 1:
 	local fontsList = core.FontProvider:GetFontsName();
 	local font_opts = {
 		["name"]  ="custom_font_name",
 		["parent"] = UIConfig,
 		["title"] = L_OPTIONS_CUSTOMFONTNAME,
 		["items"] = fontsList,
-		["defaultVal"] = ListLooterDB.settings.customFontName or "Default", 
+		["defaultVal"] = ListLooterDB.settings.customFontName or "Default",
 		["changeFunc"] = function(dropdown_frame, dropdown_val)
 			ListLooterDB.settings.customFontName = dropdown_val;
 			core.Frame:UpdateSettings();
@@ -433,17 +434,17 @@ function Config:CreateMenu()
 	----------------------------------
 	UIConfig.poiner2 = self:CreatePointer(UIConfig, -320, L_OPTIONS_FRAMESETTINGS);
 	UIConfig.poiner2:SetWidth(550);
-	
+
     -- Check Button 5:
 	UIConfig.cbLootFrame = core.Override.CreateFrameA(nil, "CheckButton", nil, UIConfig, "UICheckButtonTemplate");
 	UIConfig.cbLootFrame:SetPoint("TOPLEFT", UIConfig.poiner2, "BOTTOMLEFT", 0, -5);
 	UIConfig.cbLootFrame.text:SetText(L_OPTIONS_FRAMEENABLE);
 	UIConfig.cbLootFrame:SetChecked(ListLooterDB.settings.isLootFrame);
-    UIConfig.cbLootFrame:SetScript("OnClick", function(self, button, down) 
+    UIConfig.cbLootFrame:SetScript("OnClick", function(self, button, down)
 												ListLooterDB.settings.isLootFrame = self:GetChecked() and true or false;
 												core.Frame.HideBlizzardLootFrame("isHide", ListLooterDB.settings.isLootFrame);
 										   end);
-	
+
 	-- Slider 1:
 	UIConfig.slider1 = self:CreateSlider(UIConfig, L_OPTIONS_FRAMEICOSIZE, 10, 50, ListLooterDB.frame.iconSize, "TOPLEFT", UIConfig.cbLootFrame, "BOTTOMLEFT", 0, -20);
 	UIConfig.slider1:SetScript('OnValueChanged', function(self, value)
@@ -454,7 +455,7 @@ function Config:CreateMenu()
 
 			core.Frame.UpdateSettings();
 		end)
-	
+
 	-- Slider 2:
 	UIConfig.slider2 = self:CreateSlider(UIConfig, L_OPTIONS_FRAMEFONTITEMSIZE, 8, 20, ListLooterDB.frame.fontSizeItem, "TOPLEFT", UIConfig.cbLootFrame, "BOTTOMLEFT", 300, -20);
 	UIConfig.slider2:SetScript('OnValueChanged', function(self, value)
@@ -465,7 +466,7 @@ function Config:CreateMenu()
 
 			core.Frame.UpdateSettings();
 		end)
-	
+
 	-- Slider 3:
 	UIConfig.slider3 = self:CreateSlider(UIConfig, L_OPTIONS_FRAMEFONTCOUNTSIZE, 8, 20, ListLooterDB.frame.fontSizeCount, "TOPLEFT", UIConfig.slider1, "BOTTOMLEFT", 0, -40);
 	UIConfig.slider3:SetScript('OnValueChanged', function(self, value)
@@ -476,29 +477,35 @@ function Config:CreateMenu()
 
 			core.Frame.UpdateSettings();
 		end)
-	
+
 	InterfaceOptions_AddCategory(UIConfig);
-	
+
 	----------------------------------
 	-- LIST SETTINGS
 	----------------------------------
 	UIConfig.list = core.Override.CreateFrameA(nil, "Frame", "ListLooterConfigList", InterfaceOptionsFramePanelContainer);
 	UIConfig.list.name = L_OPTIONS_LISTPANELNAME;
 	UIConfig.list.parent = UIConfig.name;
-	
+	UIConfig.list:SetScript("OnEnter", function()
+			if (previouslyItemListRow) then
+				previouslyItemListRow.delete:Hide();
+				previouslyItemListRow = nil;
+			end
+		end);
+
 	-- Title:
 	UIConfig.list.title = UIConfig.list:CreateFontString(nil, "BACKGROUND", "GameFontNormalLarge");
     UIConfig.list.title:SetPoint("TOPLEFT", UIConfig.list, "TOPLEFT", 40, -20);
     UIConfig.list.title:SetText(L_OPTIONS_LISTPANELNAME);
-	
+
 	-- Desctiption:
 	UIConfig.list.description = UIConfig.list:CreateFontString(nil, "BACKGROUND", "GameFontNormal");
 	UIConfig.list.description:SetPoint("TOPLEFT", UIConfig.list, "TOPLEFT", 40, -55);
     UIConfig.list.description:SetText(L_OPTIONS_LISTADDTEXT);
-	
+
 	-- Edit text:
 	UIConfig.list.ebItemId = self:CreateEditBox(UIConfig.list, -75, false);
-	UIConfig.list.ebItemId:SetScript("OnEnterPressed", 
+	UIConfig.list.ebItemId:SetScript("OnEnterPressed",
 									function(self)
 										local newId = self:GetText();
 										self:SetText('');
@@ -513,34 +520,34 @@ function Config:CreateMenu()
 	UIConfig.list.listFrame:SetAllPoints(UIConfig.list);
 	UIConfig.list.listFrame:SetPoint("TOPLEFT", 35, -100);
 	UIConfig.list.listFrame:SetPoint("BOTTOMRIGHT", -27, 30);
-	
-	core.Override.ApplyBackdropA(nil, UIConfig.list.listFrame, {bgFile = "Interface/Tooltips/UI-Tooltip-Background", 
-                                            edgeFile = "Interface/Tooltips/UI-Tooltip-Border", 
+
+	core.Override.ApplyBackdropA(nil, UIConfig.list.listFrame, {bgFile = "Interface/Tooltips/UI-Tooltip-Background",
+                                            edgeFile = "Interface/Tooltips/UI-Tooltip-Border",
                                             tile = true, tileSize = 16, edgeSize = 16});
 	UIConfig.list.listFrame:SetBackdropColor(0,0,0,0.2);
-	
-	--scrollframe 
+
+	--scrollframe
 	UIConfig.list.listFrame.ScrollFrame = core.Override.CreateFrameA(nil, "ScrollFrame", nil, UIConfig.list.listFrame, "UIPanelScrollFrameTemplate");
 	UIConfig.list.listFrame.ScrollFrame:SetPoint("TOPLEFT", UIConfig.list.listFrame, "TOPLEFT", 4, -8);
 	UIConfig.list.listFrame.ScrollFrame:SetPoint("BOTTOMRIGHT", UIConfig.list.listFrame, "BOTTOMRIGHT", -3, 4);
 	UIConfig.list.listFrame.ScrollFrame:SetClipsChildren(true);
 	UIConfig.list.listFrame.ScrollFrame:SetScript("OnMouseWheel", ScrollFrame_OnMouseWheel);
-	
+
 	UIConfig.list.listFrame.ScrollFrame.ScrollBar:ClearAllPoints();
     UIConfig.list.listFrame.ScrollFrame.ScrollBar:SetPoint("TOPLEFT", UIConfig.list.listFrame.ScrollFrame, "TOPRIGHT", -12, -18);
     UIConfig.list.listFrame.ScrollFrame.ScrollBar:SetPoint("BOTTOMRIGHT", UIConfig.list.listFrame.ScrollFrame, "BOTTOMRIGHT", -7, 18);
-	
-	--content frame 
-	UIConfig.list.listFrame.ScrollFrame.content = core.Override.CreateFrameA(nil, "Frame", nil, UIConfig.list.listFrame.ScrollFrame) 
-	UIConfig.list.listFrame.ScrollFrame.content:SetSize(550, 128) 
+
+	--content frame
+	UIConfig.list.listFrame.ScrollFrame.content = core.Override.CreateFrameA(nil, "Frame", nil, UIConfig.list.listFrame.ScrollFrame)
+	UIConfig.list.listFrame.ScrollFrame.content:SetSize(550, 128)
 	UIConfig.list.listFrame.ScrollFrame:SetScrollChild(UIConfig.list.listFrame.ScrollFrame.content)
 	UIConfig.list.listFrame.ScrollFrame.content.rows = {};
-	
+
 	Config:CreateContent(UIConfig.list.listFrame.ScrollFrame.content);
-	
+
 	InterfaceOptions_AddCategory(UIConfig.list);
 	InterfaceAddOnsList_Update();
-	
+
 	return UIConfig;
 end
 
@@ -572,7 +579,7 @@ function Config:CreateDropdown(opts)
         local info = UIDropDownMenu_CreateInfo()
         for key, val in pairs(menu_items) do
             info.text = key;
-            info.checked = (ListLooterDB.settings.customFontName == val)
+            info.checked = (ListLooterDB.settings.customFontName == key)
             info.menuList= key
             info.hasArrow = false
             info.func = function(b)
@@ -624,16 +631,16 @@ function Config:CreateFontString(parent, text, template,  ...)
 end
 
 function Config:AddItemByLink(itemLink)
-	if itemLink ~= nil then 
+	if itemLink ~= nil then
 		local _, _, Id = string.find(itemLink, "item:(%d+):")
-			
-		if Id then 
+
+		if Id then
 			table.insert(ListLooterDB.LootDB,Id);
 			Config:GetListFromGlobal();
 			Config:CreateContent(UIConfig.list.listFrame.ScrollFrame.content);
 			print("ListLooter: "..itemLink.." added");
 		end
-	end 
+	end
 end
 
 --------------------------------------
--- fontProvider.lua
+++ fontProvider.lua
@@ -7,63 +7,75 @@ local LSM3 = LibStub("LibSharedMedia-3.0", true)
 local LSM2 = LibStub("LibSharedMedia-2.0", true)
 local SML = LibStub("SharedMedia-1.0", true)
 
-core.FontProvider = {}; -- adds Config table to addon namespace
+core.FontProvider = {}; -- adds FontProvider table to addon namespace
 
 local FontProvider = core.FontProvider;
 local fontFolderName = "Interface\\AddOns\\!ListLooter\\Fonts\\";
 local defaultFont = GameFontWhite:GetFont();
 
+--Fonts if not LSM used
 local fontsList =
 {
     Default = defaultFont,
     Enigmatic = fontFolderName.."EnigmaU_2.ttf"
 }
 
-----------------------------------
+--------------------------------------
 -- FontProvider functions
 --------------------------------------
 
 function FontProvider:Init()
-    if (not LSM) then 
-        if (not SML) then  
+    if (not LSM) then
+        if (not SML) then
             SML = LibStub("SharedMedia-1.0", true);
-
-            if (SML) then 
+            if (SML) then
                 LSM = SML;
-                LSM:Register(LSM.MediaType.FONT, "Enigmatic", fontFolderName.."EnigmaU_2.ttf");
             end
         else
             LSM = SML;
         end
 
-        if (not LSM2) then 
+        if (not LSM2) then
             LSM2 = LibStub("LibSharedMedia-2.0", true);
-            if (LSM2) then 
+            if (LSM2) then
                 LSM = LSM2;
-                LSM:Register(LSM.MediaType.FONT, "Enigmatic", fontFolderName.."EnigmaU_2.ttf");
             end
         else
             LSM = LSM2;
         end
 
-        if (not LSM3) then 
+        if (not LSM3) then
             LSM3 = LibStub("LibSharedMedia-3.0", true)
-            if (LSM3) then 
+            if (LSM3) then
                 LSM = LSM3;
-                LSM:Register(LSM.MediaType.FONT, "Enigmatic", fontFolderName.."EnigmaU_2.ttf", LSM.LOCALE_BIT_ruRU);
             end
         else
             LSM = LSM3;
         end
 
-        if (LSM) then 
+        if (LSM) then
+            FontProvider:RegisterCustomFonts();
             core.Config.LSMDetected();
         end
     end
 end
 
+function FontProvider:RegisterCustomFonts()
+    local customFontsList = {
+        Enigmatic = {fontFolderName.."EnigmaU_2.ttf", LSM.LOCALE_BIT_ruRU+LSM.LOCALE_BIT_western}
+    }
+
+    for key, val in pairs(customFontsList) do
+        if (LSM == LSM3) then
+            LSM:Register(LSM.MediaType.FONT, key, val[1], val[2]);
+        else
+            LSM:Register(LSM.MediaType.FONT, key, val[1]);
+        end
+    end
+end
+
 function FontProvider:GetFontsName()
-    if (LSM) then 
+    if (LSM) then
         return LSM:HashTable(LSM.MediaType.FONT);
     else
         return fontsList;
